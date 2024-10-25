a1059eecb79e39c0e25bbe06b297ff7f2208da6c
EkaterinaMiller <evmiller@nic.edu>
2024-10-25 10:14:38 -0700

    fix deletion

--- main.cpp
+++ main.cpp
@@ -15,7 +15,10 @@ int add(int a, int b);
 void say(string word);
 int main()
 {
-
+    cout<<"My name is Tom\n";
+    cout<<"hello world\n";
+    cout<<"5 + 7 = "<<add(5,7)<<endl;
+    say("Hi");
     return 0;
 }
 int add(int a, int b)
